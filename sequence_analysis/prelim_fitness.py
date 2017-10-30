afb61b03708b15276103b92a7de1797180a6d652
Christa.Caggiano <Christa.Caggiano@otp590692ots.ucsf.edu>
2017-10-30 11:47:08 -0700

    code for making preliminary fitness plots

--- sequence_analysis/prelim_fitness.py
+++ sequence_analysis/prelim_fitness.py
@@ -1,16 +1,20 @@
-
-import pickle
-import pandas
+# imports
+#############################################################
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from Bio import SeqIO
+import plotly.plotly as py
+import plotly.graph_objs as go
 
+# load and manipulate dataframe
+#############################################################
 
-# load in dataframe
-d_rotation1 = pickle.load(open("buds_fit_df1.pkl", "rb"))
+# load in dataframe of relative fitness values
+d_rotation1 = pd.read_csv("jared_relative_fitness.csv")
 
-# transpose - maybe useful?
+# transpose - useful for some plotting applications
 d_rotation2 = d_rotation1.transpose()
 
 # turn -10 NaNs to actual NaNs for seaborn to mask in heatmap
@@ -21,46 +25,67 @@ mask1 = d_nan.isnull()
 # just the c-terminus
 d_nan_c_term = d_rotation1.drop(d_nan.columns[range(0,90)], axis=1).replace(-10, np.NaN)
 mask2 = d_nan_c_term.isnull()
-#
-res_of_interest = [120, 121, 122, 127, 128, 129, 134]
 
-mean_fit_interesting = []
-for res in res_of_interest:
-    mean_fit_interesting.append(d_nan[res].mean())
+# collapsing dataframe
+#############################################################
 
+# find mean fitness for all values in the protein
 mean_fit_all = []
 for column in d_nan:
     mean_fit_all.append(d_nan[column].mean())
 
+# maximum fitness for each residue
 max_fit = []
 for column in d_nan:
     max_fit.append(d_nan[column].max())
 
+# min fitness for each residue
 min_fit = []
 for column in d_nan:
     min_fit.append(d_nan[column].min())
 
+# plotting
+#############################################################
+
+# loads in wt sequence and changes to amino acid seq
 wt = SeqIO.read("asyn_seq.txt", "fasta")
-wt_aa = list((wt.seq.translate()))
+wt_aa = ((wt.seq.translate()))
 
-# print(wt_aa[90:])
-
+# gets just the indices for charged amino acids
 indices_neg = [i for i, x in enumerate(wt_aa) if x == "E" or x == "D"]
 indices_pos = [i for i, x in enumerate(wt_aa) if x == "R" or x == "K"]
-print(indices_neg)
 
+# subsets to just the fitness scores for aa's that are originally and saves as csv
+pos_neg = d_rotation1[["E", "D", "R", "K"]]
+pd.pos_neg.to_csv("pos_neg.csv")
 
+# finds the neg residues and finds instances where neg residues are changed to positive
+neg = d_rotation1.loc[indices_neg]
+neg_fitness_to_pos = neg[["R", "K"]].replace(-10, np.NaN)
 
-# print(np.argwhere(np.isnan(min_fit)))
-# print(len(wt_list))
-#
-# plt.plot(res_of_interest, mean_fit_interesting, 'o')
-# plt.plot(list(range(140)), max_fit)
-# plt.plot(list(range(140)), mean_fit_all)
-# plt.plot(list(range(140)), min_fit)
-# plt.show()
+# finds positive residues and gets fitness of instances of positive changed to negative
+pos = d_rotation1.loc[indices_pos]
+pos_fitness_to_neg = pos[["E", "E"]].replace(-10, np.NaN)
 
+# sends to plotly to make a heatmap of negative values changed to positive
+data = [go.Heatmap( z=neg_fitness_to_pos.values.tolist(), colorscale='Viridis')]
+py.iplot(data, filename='neg to pos')
 
-sns.heatmap(d_rotation1, vmin=-8, vmax=2, mask=mask1, cmap="coolwarm")
+# makes plotly heatmap of fitness pos to negative
+data = [go.Heatmap(z=pos_fitness_to_neg.values.tolist(), colorscale='Viridis')]
+py.iplot(data, filename='pos to neg')
+
+# plots mean and max fitness for each residue
+plt.plot(list(range(140)), max_fit)
+plt.plot(list(range(140)), mean_fit_all)
+plt.plot(list(range(140)), min_fit)
+plt.show()
+
+# dark style heatmap of all fitness values
+sns.set_style("dark")
+ax = sns.heatmap(d_rotation1, vmin=-2, vmax=2, mask=mask1, cmap=sns.diverging_palette(250, 15, s=75, l=40, center="dark", as_cmap=True), linewidths=.5)
+ax = sns.heatmap(d_rotation1, vmin=-2, vmax=2, mask=mask1, cmap="hot", linewidths=.5)
+plt.xlabel("residue number")
+plt.ylabel("mutation")
 plt.show()
 
